44800c818e5043182f664c9d8e80ba6afb234fae
yimingxx <yx2900@columbia.edu>
2025-04-22 17:12:41 -0400

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import logo from './logo.svg';
-import FontMatchQuestion from './FontMatchQuestion';
 
 function App() {
-  const [page, setPage] = useState('home'); 
-
   const message =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris bibendum orci quam, non mollis erat aliquam a.';
 
@@ -19,7 +16,7 @@ function App() {
   const [deleting, setDeleting] = useState(false);
 
   useEffect(() => {
-    if (isPaused || page !== 'home') return; 
+    if (isPaused) return;
 
     const delay = deleting ? 30 : 60;
 
@@ -47,29 +44,8 @@ function App() {
     }, delay);
 
     return () => clearTimeout(timeout);
-  }, [index, deleting, isPaused, page]);
+  }, [index, deleting, isPaused]);
 
-  
-  if (page === 'quiz1') {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} alt="Logo" className="logo" />
-          <nav className="nav">
-            <a href="#">Font Types</a>
-            <a href="#">Parts of a Letter</a>
-            <button className="learn-btn" onClick={() => setPage('home')}>
-              Back
-            </button>
-          </nav>
-        </header>
-
-        <FontMatchQuestion />
-      </div>
-    );
-  }
-
-  
   return (
     <div className="App">
       <header className="App-header">
@@ -78,9 +54,7 @@ function App() {
           <a href="#">Font Types</a>
           <a href="#">Parts of a Letter</a>
           <button className="learn-btn">Learn</button>
-          <button className="quiz-btn" onClick={() => setPage('quiz1')}>
-            Quiz Yourself
-          </button>
+          <button className="quiz-btn">Quiz Yourself</button>
         </nav>
       </header>
 
