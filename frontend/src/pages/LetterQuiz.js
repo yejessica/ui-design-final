cd3da7a384c740b6eb6b0096e640dde720585cde
yimingxx <yx2900@columbia.edu>
2025-05-12 15:37:46 -0400

    LetterQuiz.js
    Fixed progress bar

--- frontend/src/pages/LetterQuiz.js
+++ frontend/src/pages/LetterQuiz.js
@@ -47,7 +47,7 @@ const LetterQuiz = ({ onNext }) => {
       {/* Progress Bar */}
       <div className="fixed bottom-4 left-4 w-1/4">
         <div className="w-full bg-gray-200 rounded-full h-2">
-          <div className="bg-green-400 h-2 rounded-full" style={{ width: '60%' }} />
+          <div className="bg-green-400 h-2 rounded-full" style={{ width: '40%' }} />
         </div>
       </div>
 
